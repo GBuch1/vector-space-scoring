01f1726ff7326ff372cdb0b650e4ac02674983d0
GBuch1 <143757443+GBuch1@users.noreply.github.com>
2023-11-09 21:41:50 -0800

    Update test_vector_space_models_TestCorpus.py

--- src/tests/test_vector_space_models_TestCorpus.py
+++ src/tests/test_vector_space_models_TestCorpus.py
@@ -1,25 +1,33 @@
+from math import log10
 import unittest
-from vectorspace.vector_space_models import Document, Corpus
+from vectorspace.vector_space_models import Document, Corpus, Vector
 
 
 class TestCorpus(unittest.TestCase):
     def setUp(self):
-        self.new_doc1 = Document(title = "doc1", words=["freedom", "liberty", "liberty"])
-        self.new_doc2 = Document(title = "doc2", words=["liberty", "yelling", "crawled"])
-        self.new_doc3 = Document(title = "doc3", words=["justice", "drugs", "oil"])
-        self.documents = [self.new_doc1, self.new_doc2, self.new_doc3]
-        self.corpus1 = Corpus([self.new_doc1, self.new_doc2])
-        self.corpus2 = Corpus(self.documents)
+        self.new_doc1 = Document(title="doc1", words=["freedom", "liberty", "liberty"])
+        self.new_doc2 = Document(title="doc2", words=["liberty", "yelling", "crawled"])
+        self.new_doc3 = Document(title="doc3", words=["justice", "drugs", "oil"])
+        self.new_doc4 = Document(title="doc4", words=["fleeing", "yelling", "crawled"])
+        self.new_doc5 = Document(title="doc5", words=["running", "jumping", "fleeing"])
+        self.new_doc6 = Document(title="doc_test1", words=["running", "jumping"])
+        self.new_doc7 = Document(title="doc-test2", words=["running", "crying"])
+        self.corpus1 = Corpus(documents=[self.new_doc1, self.new_doc2, self.new_doc4, self.new_doc5])
+        self.corpus2 = Corpus(documents=[self.new_doc1, self.new_doc2, self.new_doc3])
+        self.corpus3 = Corpus(documents=[self.new_doc6, self.new_doc7])
 
     def test_compute_terms(self):
         terms = self.corpus1._compute_terms()
-        self.assertEqual(terms, {"freedom", "liberty", "yell", "crawl"})
+        expected_outcome = {'crawled': 0, 'fleeing': 1, 'freedom': 2, 'jumping': 3, 'liberty': 4, 'running': 5,
+                            'yelling': 6}
+        for term in terms:
+            self.assertIn(term, expected_outcome)
 
     def test_compute_df(self):
         df1 = self.corpus1._compute_df("liberty")
-        df2 = self.corpus2._compute_df("liberty")
+        df2 = self.corpus2._compute_df("drugs")
         self.assertEqual(df1, 2)
-        self.assertEqual(df2, 2)
+        self.assertEqual(df2, 1)
 
     # def test_check_membership(self):
     # For some reason I can't access the check_membership function
@@ -32,10 +40,22 @@ class TestCorpus(unittest.TestCase):
         # a term that occurs once in a corpus with a size of 3 documents. To do that
         # follow the formula provided in the Google Doc for this assignment
         # the answer is log10(1 + 1) * log10(3 / (1 + 1)) = .053008751
-        corpus = Corpus(self.documents)
-        score = corpus._compute_tf_idf("oil", doc=self.documents[0])
-        self.assertEqual(score, .053008751)
+        score = self.corpus2._compute_tf_idf("oil", self.new_doc3)
+        self.assertEqual(score, 0.053008750949996714)
 
+    def test_compute_tf_idf_vector(self):
+        test_value1 = self.corpus3.compute_tf_idf_vector(self.new_doc1)
+        expected_value1 = Vector([0, -0.08401688247, -0.08401688247])
+        self.assertAlmostEqual(test_value1[0], expected_value1[0])
+
+    def test_compute_tf_idf_matrix(self):
+        # "running", "jumping"
+        # "running", "crying"
+        expected = {
+            "doc_test1": Vector([-0.05300875094999672, 0.0]),
+            "doc_test2": Vector([-0.05300875094999672, 0.0])}
+        computed = self.corpus3._compute_tf_idf_matrix()
+        self.assertEqual(expected, computed)
 
 if __name__ == '__main__':
     unittest.main()
